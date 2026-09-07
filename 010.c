f0d483268a436b3933256ba43d644c3bb196e7ef
saimasafwat1232-tech <saimasafwat1232@gmail.com>
2026-09-07 12:15:28 +0600

    Add files via upload

---
+++ 010.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    a = 1000;
+    printf("Value of a is %d\n", a);
+
+    a = -21000;
+    printf("Value of a is %d\n", a);
+
+    a = 100000000;
+    printf("Value of a is %d\n", a);
+
+    a = -10000000000;
+    printf("Value of a is %d\n", a);
+
+    a = 10000000200003526;
+    printf("Value of a is %d\n", a);
+
+    a = -433456782251;
+    printf("Value of a is %d\n", a);
+
+    return 0;
+    /*-2147483648 to 2147483647 c program a sudu ai gulu likha jai*/
+}
