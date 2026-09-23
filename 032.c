0703f0654b387102f255572e0c312f0b92e8f4ed
saimasafwat1232-tech <saimasafwat1232@gmail.com>
2026-09-23 21:05:26 +0600

    Add files via upload

---
+++ 032.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n = 5;
+    int i =1;
+    for ( ; ; )
+    {
+        if(i > 10){
+            break;
+        }
+        printf("%d * %d = %d\n", n, i, n*i);
+        i = i + 1;
+    }
+    return 0;
+    
+}
