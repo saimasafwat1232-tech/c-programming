594a9f7a74fa23579573fda510771f5f71ef09c9
saimasafwat1232-tech <saimasafwat1232@gmail.com>
2026-09-26 19:31:45 +0600

    Add files via upload

---
+++ 047.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, sum;
+    scanf("%d", &n);
+    for(i = 1, sum = 0; i <= n; i++){
+        sum = sum + i;
+    }
+    printf("summation is %d\n", sum);
+
+    return 0;
+
+}
